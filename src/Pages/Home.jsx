82895cd2148bc1958005b7708d27b42567d72d48
bhanu567 <bhanupjha5465@gmail.com>
2024-04-01 05:22:21 +0530

    Added Log out functionality

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export const key = "AIzaSyCIYs9Qg0Yl5Pg2YeA-3spg45Dgs7TJi_c";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const logOutHandler = () => {
+    localStorage.removeItem("idToken");
+    navigate("/login", { replace: true });
+  };
   return (
     <div>
       <div
@@ -18,25 +23,39 @@ const Home = () => {
         }}
       >
         <i style={{ fontSize: "30px" }}>WelCome to Expense Tracker!!!</i>
-        <i
-          style={{
-            borderRadius: "10px",
-            backgroundColor: "rgba(255, 179, 0, 0.513)",
-            padding: "5px 10px",
-          }}
-        >
-          Your Profile is Incomplete.{" "}
-          <NavLink
-            to="/updateprofile"
+        <div>
+          <button
+            type="button"
+            class="btn btn-danger btn-sm me-2"
             style={{
-              textDecoration: "none",
-              fontStyle: "italic",
-              cursor: "pointer",
+              borderRadius: "10px",
+              padding:'3px 20px'
+            }}
+            onClick={logOutHandler}
+          >
+            Logout
+          </button>
+
+          <i
+            style={{
+              borderRadius: "10px",
+              backgroundColor: "rgba(255, 179, 0, 0.513)",
+              padding: "5px 10px",
             }}
           >
-            Complete now
-          </NavLink>
-        </i>
+            Your Profile is Incomplete.{" "}
+            <NavLink
+              to="/updateprofile"
+              style={{
+                textDecoration: "none",
+                fontStyle: "italic",
+                cursor: "pointer",
+              }}
+            >
+              Complete now
+            </NavLink>
+          </i>
+        </div>
       </div>
       <hr style={{ margin: "0px" }} />
     </div>
